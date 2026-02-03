1e72510cd92dba859bd2455a31adf8d37325ffd7
tech-ml-quant-luv <luvratan.techengineer@gmail.com>
2026-02-04 00:26:10 +0530

    refresh token fastapi endpoint added

---
+++ access_token/create_access_token.py
@@ -0,0 +1,44 @@
+from fyers_apiv3 import fyersModel
+import json
+
+client_id = "ALT6RUE1IF-100"
+secret_key = "0UT0LW5PE4"
+redirect_uri = "https://luvratan.tech/"
+
+def get_auth_url():
+    """Generate auth URL for FYERS login"""
+    session = fyersModel.SessionModel(
+        client_id=client_id,
+        secret_key=secret_key,
+        redirect_uri=redirect_uri,
+        response_type="code",
+        state="sample_state"
+    )
+    return session.generate_authcode()
+
+def generate_token_from_auth_code(auth_code: str):
+    """Exchange auth_code for access_token"""
+    session = fyersModel.SessionModel(
+        client_id=client_id,
+        secret_key=secret_key,
+        redirect_uri=redirect_uri,
+        response_type="code",
+        grant_type="authorization_code"
+    )
+    
+    session.set_token(auth_code)
+    token_response = session.generate_token()
+    
+    # Save to JSON file
+    with open("fyers_token.json", "w") as f:
+        json.dump(token_response, f, indent=2)
+    
+    return token_response
+
+def get_token_from_file():
+    """Read token from JSON file"""
+    try:
+        with open("fyers_token.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return None
