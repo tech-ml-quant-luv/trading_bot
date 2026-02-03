c799a65e267125a12aae3905cb7e301ead6ba2f3
tech-ml-quant-luv <luvratan.techengineer@gmail.com>
2026-02-03 10:29:24 +0530

    connect

--- job_runner.py
+++ job_runner.py
@@ -1,20 +1,20 @@
 from datetime import datetime
-from create_features import create_features
-from fetch_ohlcv_data import fyers_history_to_df
+from data.create_features import create_features
+from data.fetch_ohlcv_data import fyers_history_to_df
 from concurrent.futures import ThreadPoolExecutor
 
 TICKERS = ["ADANIPORTS", "ICICIBANK", "INFY", "RELIANCE"]
 
 def pipeline_function(ticker):
     start_time = datetime.now()
-    print(f"{ticker}Job started at {start_time}")
+    print(f"{ticker}, Job started at {start_time}")
 
     #Fetch data for one ticker
     df = fyers_history_to_df(ticker)
     final_df = create_features(df)
 
     print(f"{ticker} Latest Row: {final_df.tail(1)}")
-    final_df.to_csv(f"{ticker}.csv", index=False)
+    final_df.to_csv(f"./data/{ticker}.csv", index=False)
 
     print(f"Job completed at {datetime.now()}")
 
