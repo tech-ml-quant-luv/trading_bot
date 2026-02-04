be265f1a5f74504793d8cfa894dbd485356fef67
tech-ml-quant-luv <luvratan.techengineer@gmail.com>
2026-02-04 16:30:58 +0530

    removed unnecessary conditions for strategy and added new ML models

--- engine/trigger.py
+++ engine/trigger.py
@@ -17,11 +17,11 @@ def log_trigger(ticker, signal_type, ltp, support, resistance, ma20, ma50, ma200
     conn.close()
 
 def trigger_long(ltp, ma20, ma50, ma200, rsi, ticker, support, resistance, ml_prediction, ml_confidence):
-    if (ltp >= ma20) and (35 <= rsi <= 55) and (ma50 > ma200):
+    if (ltp >= ma20):
         print(f"Long entry triggered for {ticker} at {datetime.datetime.now()}")
         log_trigger(ticker, 'LONG', ltp, support, resistance, ma20, ma50, ma200, rsi, ml_prediction, ml_confidence, 'ltp<=support & conditions met')
 
 def trigger_short(ltp, ma20, ma50, ma200, rsi, ticker, support, resistance, ml_prediction, ml_confidence):
-    if (ltp <= ma20) and (60 <= rsi <= 75) and (ma50 < ma200):
+    if (ltp <= ma20):
         print(f"Short entry triggered for {ticker} at {datetime.datetime.now()}")
         log_trigger(ticker, 'SHORT', ltp, support, resistance, ma20, ma50, ma200, rsi, ml_prediction, ml_confidence, 'ltp>=resistance & conditions met')
